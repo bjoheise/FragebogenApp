fb2da42684edbd118009b892d62f61aeaa886f6c
bjoheise <122088068+bjoheise@users.noreply.github.com>
2023-01-10 12:42:00 +0100

    Add files via upload

---
+++ BerechnungenTest.java
@@ -0,0 +1,51 @@
+package Fragebogen;
+
+import java.util.Scanner;
+
+public class BerechnungenTest {
+
+    static int sel = 0;
+    static int kel = 0;
+    static int bw = 0;
+    int er = 0;
+    int nki = 0;
+    int aki = 0;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        berechnung();
+    }
+
+    public static void berechnung(){
+
+        int antwort1, antwort2, antwort3;
+
+        System.out.println("Ich kann gut zuhören. (j=1/n=0)");
+        antwort1 = scanner.nextInt();
+        System.out.println("Ich neige dazu, in Gruppen tonangebend sein zu wollen. (j=1/n=0)");
+        antwort2 = scanner.nextInt();
+        System.out.println("Es scheint, dass ich anderen bald einmal widerspreche. (j=1/n=0)");
+        antwort3 = scanner.nextInt();
+
+        if (antwort1==1){
+            bw++;
+        }
+        if (antwort2==1){
+            bw++;
+            kel++;
+        }
+        if (antwort3==1){
+            bw++;
+            kel++;
+        }
+
+        sel=bw-kel;
+
+        System.out.println("sel =" + sel);
+        System.out.println("kel =" + kel);
+
+    }
+
+
+}
