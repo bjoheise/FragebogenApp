6752169beb05b9dcf1ae45ebda2fb11967ad9ca3
Bloxxor <f.gabele@bytebenders.de>
2023-01-24 12:20:36 +0100

    Chart add 30% and 70% lines

--- src/Controller/Coach/ResultGraphSceneController.java
+++ src/Controller/Coach/ResultGraphSceneController.java
@@ -3,30 +3,11 @@ package Fragebogen.Controller.Coach;
 import Fragebogen.Egogram;
 import Fragebogen.Model.Calculation;
 import Fragebogen.Modules.ResultToPdf;
-import javafx.collections.FXCollections;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
-import javafx.scene.image.WritableImage;
-import javafx.stage.Stage;
-
-import javax.imageio.ImageIO;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ResultGraphSceneController {
-
-    public Button buttonExport;
-
-    public Stage stage = Egogram.instance.primaryStage;
 
     public void onButtonBackClick(ActionEvent actionEvent) throws IOException {
         Egogram.instance.loadScene("Coach/IntroSceneCoach.fxml");
--- src/Modules/ResultToPdf.java
+++ src/Modules/ResultToPdf.java
@@ -3,11 +3,14 @@ package Fragebogen.Modules;
 import Fragebogen.Model.Calculation;
 import Fragebogen.Model.Question;
 import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.List;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.properties.ListNumberingType;
@@ -19,6 +22,8 @@ import javafx.scene.chart.*;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.image.WritableImage;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 
 import javax.imageio.ImageIO;
 import java.io.File;
@@ -28,22 +33,10 @@ import java.util.Arrays;
 
 public class ResultToPdf {
 
+    private Line valueMarker = new Line();
+
     public static final String PDF_DEST = "./target/egogram-export.pdf";
     public static final String TEMP_IMG_DEST = "./target/egogram.png";
-
-    /**
-     * @param node
-     * @param questionObservableList
-     * @throws Exception
-     */
-    public void generatePdf(Node node, ObservableList<Question> questionObservableList) throws Exception {
-
-        File file = new File(PDF_DEST);
-        file.getParentFile().mkdirs();
-
-        // this.manipulatePdf(PDF_DEST, questionObservableList);
-
-    }
 
     public void manipulatePdf(ArrayList<Integer> scaleValues) throws Exception {
 
@@ -72,13 +65,12 @@ public class ResultToPdf {
 
         // Generate a PNG of the Egogram to put it into the pdf
         this.saveAsPng(scaleValues);
-        // Create an ImageData object
-//        ImageData data = ImageDataFactory.create(TEMP_IMG_DEST);
-//        Image img = new Image(data);
+        ImageData data = ImageDataFactory.create(TEMP_IMG_DEST);
+        Image img = new Image(data);
 
         // Add the elements
         doc.add(p);
-//        doc.add(img);
+        doc.add(img);
         doc.add(list);
 
         // Close the PDF
@@ -91,31 +83,31 @@ public class ResultToPdf {
         // }
 
         // Check if the file exists
-        File pdfFile = new File(PDF_DEST);
-
-        if (pdfFile.exists() && !pdfFile.isDirectory()) {
-
-            Alert alert = new Alert(
-                    Alert.AlertType.CONFIRMATION,
-                    "Datei exportiert",
-                    ButtonType.OK
-            );
-
-            // Display the Alert-Popup
-            alert.showAndWait();
-
-        } else {
-
-            Alert alert = new Alert(
-                    Alert.AlertType.ERROR,
-                    "Fehler!",
-                    ButtonType.OK
-            );
-
-            // Display the Alert-Popup
-            alert.showAndWait();
-
-        }
+//        File pdfFile = new File(PDF_DEST);
+//
+//        if (pdfFile.exists() && !pdfFile.isDirectory()) {
+//
+//            Alert alert = new Alert(
+//                    Alert.AlertType.CONFIRMATION,
+//                    "Datei exportiert",
+//                    ButtonType.OK
+//            );
+//
+//            // Display the Alert-Popup
+//            alert.showAndWait();
+//
+//        } else {
+//
+//            Alert alert = new Alert(
+//                    Alert.AlertType.ERROR,
+//                    "Fehler!",
+//                    ButtonType.OK
+//            );
+//
+//            // Display the Alert-Popup
+//            alert.showAndWait();
+//
+//        }
 
     }
 
@@ -124,11 +116,18 @@ public class ResultToPdf {
      */
     public void saveAsPng(ArrayList<Integer> scaleValues) throws IOException {
 
-        File file = new File("./target/egogram.png");
+        File file = new File(TEMP_IMG_DEST);
+
+        // create a line at 30%
+        Line line70 = new Line(76.0f, 130.0f, 567.0f, 130.0f);
+        // create a line at 70%
+        Line line30 = new Line(76.0f, 251.0f, 567.0f, 251.0f);
 
         Scene scene = new Scene(new Group(), 600, 400);
 
         ((Group) scene.getRoot()).getChildren().add(this.buildChart());
+        ((Group) scene.getRoot()).getChildren().add(line30);
+        ((Group) scene.getRoot()).getChildren().add(line70);
 
         // Saving the scene as image
         WritableImage image = scene.snapshot(null);
@@ -148,8 +147,6 @@ public class ResultToPdf {
         ArrayList<Integer> test = Calculation.skala();
 
         int test2[] = {12, 30, 25, 3, 65};
-
-        System.out.println(test);
 
         // Defining the x-axis
         CategoryAxis xAxis = new CategoryAxis();
@@ -173,7 +170,8 @@ public class ResultToPdf {
         barChart.setLegendVisible(false);
 
         xAxis.setCategories(FXCollections.<String>observableArrayList(Arrays.asList
-                ("1", "2")));
+                ("1", "2", "3", "4", "5"))
+        );
 
         XYChart.Series<String, Number> series1 = new XYChart.Series<>();
         XYChart.Series<String, Number> series2 = new XYChart.Series<>();
@@ -181,14 +179,43 @@ public class ResultToPdf {
         XYChart.Series<String, Number> series4 = new XYChart.Series<>();
         XYChart.Series<String, Number> series5 = new XYChart.Series<>();
 
-        series1.getData().add(new XYChart.Data<String, Number>("1", 12));
-        series2.getData().add(new XYChart.Data<String, Number>("1", 72));
-        series2.getData().add(new XYChart.Data<String, Number>("3", 45));
-        series3.getData().add(new XYChart.Data<String, Number>("4", 12));
-        series4.getData().add(new XYChart.Data<String, Number>("5", 34));
-        series5.getData().add(new XYChart.Data<String, Number>("1", 78));
+
+        series1.getData().add(new XYChart.Data<String, Number>("1", 30));
+        series1.getData().add(new XYChart.Data<String, Number>("1", 40));
+        series1.getData().add(new XYChart.Data<String, Number>("1", 25));
+
+        series2.getData().add(new XYChart.Data<String, Number>("2", 32));
+        series2.getData().add(new XYChart.Data<String, Number>("2", 78));
+
+        series3.getData().add(new XYChart.Data<String, Number>("3", 45));
+        series4.getData().add(new XYChart.Data<String, Number>("4", 12));
+        series5.getData().add(new XYChart.Data<String, Number>("5", 34));
+
 
         barChart.getData().addAll(series1, series2, series3, series4, series5);
+
+
+        //
+        Node n1 = barChart.lookup(".data0.chart-bar");
+        n1.setStyle("-fx-bar-fill: red");
+        Node n2 = barChart.lookup(".data1.chart-bar");
+        n2.setStyle("-fx-bar-fill: blue");
+        Node n3 = barChart.lookup(".data2.chart-bar");
+        n3.setStyle("-fx-bar-fill: yellow");
+
+
+//        if (series1.getYValue().intValue() > 8) {
+//            node.setStyle("-fx-bar-fill: -fx-exceeded;");
+//        } else if (data.getYValue().intValue() > 5) {
+//            node.setStyle("-fx-bar-fill: -fx-achieved;");
+//        } else {
+//            node.setStyle("-fx-bar-fill: -fx-not-achieved;");
+//        }
+
+
+        barChart.setPrefSize(580, 400);
+        //
+
 
         return barChart;
 
